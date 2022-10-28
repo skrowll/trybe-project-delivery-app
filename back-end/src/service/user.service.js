5ded60481b953d71a840c1e7ee552f6454d39779
cacau <anaclaudia.sza@outlook.com>
2022-10-28 18:44:11 -0300

    feat: status de erros

--- back-end/src/service/user.service.js
+++ back-end/src/service/user.service.js
@@ -3,20 +3,29 @@ const { User } = require('../database/models');
 const { configAuthorization } = require('../utils/Auth');
 
 const loginUser = async ({ password, email }) => {
-  const user = await User.findOne({
-    where: { email },
-    attributes: { exclude: ['id'] },
-  });
+  const user = await User.findOne({ where: { email } });
 
-  if (!user) throw new Error('Not found');
+  if (!user) {
+    const error = new Error('Invalid email');
+    error.status = 404;
+    throw error;
+  }
 
   const validPass = md5(password) === user.password;
 
-  if (!validPass) throw new Error('Invalid password');
+  if (!validPass) {
+    const error = new Error('Invalid password');
+    error.status = 400;
+    throw error;
+  }
 
   const token = configAuthorization.signAuth(user);
 
-  if (!validPass) throw new Error('Invalid token');
+  if (!validPass) {
+    const error = new Error('Invalid token');
+    error.status = 400;
+    throw error;
+  }
 
   return {
     name: user.name,

85b23ba94e07d14baf1ba7066eb8b1329f8c5089
Andre Rousselet <andre098@hotmail.com>
2022-10-28 19:48:48 -0300

    fix: comenta linhas do componente Login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
+// import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={ <Login /> } />
+      {/* <Route path="/login" element={ <Login /> } /> */}
       <Route path="/register" element={ <Register /> } />
       <Route exact path="/" element={ <Navigate to="/login" /> } />
     </Routes>

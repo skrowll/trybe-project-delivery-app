e0cee18a8c4f7a1695bc6bd398378800e6356641
cacau <anaclaudia.sza@outlook.com>
2022-10-28 17:18:05 -0300

    feat: adiciona rota login, sem middleware de erro

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,14 @@
+// require('express-async-errors'); // não instalado && aplicado
+
 const express = require('express');
+
+const userController = require('../controller/user.controller');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.post('/login', userController.loginUser);
 
 module.exports = app;
---
+++ back-end/src/service/user.service.js
@@ -0,0 +1,31 @@
+const md5 = require('md5');
+const { User } = require('../database/models');
+const { configAuthorization } = require('../utils/Auth');
+
+const loginUser = async ({ password, email }) => {
+  const user = await User.findOne({
+    where: { email },
+    attributes: { exclude: ['id'] },
+  });
+
+  if (!user) throw new Error('Invalid email');
+
+  const validPass = md5(password) === user.password;
+
+  if (!validPass) throw new Error('Invalid password');
+
+  const token = configAuthorization.signAuth(user);
+
+  if (!validPass) throw new Error('Invalid token');
+
+  return {
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token,
+  };
+};
+
+module.exports = {
+  loginUser,
+};

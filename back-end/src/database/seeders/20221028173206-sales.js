7b4d93e409f7bc32f95666f013fec88fba70d1ea
Andre Rousselet <andre098@hotmail.com>
2022-10-28 14:39:03 -0300

    feat: sales seeder

---
+++ back-end/src/database/seeders/20221028173206-sales.js
@@ -0,0 +1,26 @@
+module.exports = {
+  async up (queryInterface, _Sequelize) {
+    await queryInterface.bulkInsert('Sales', [
+      {
+        user_id: 3,
+        seller_id: 2,
+        total_price: 100,
+        delivery_address: 'Rua Um',
+        delivery_number: 'casa 1',
+        status: 'Pendente',
+      },
+      {
+        user_id: 3,
+        seller_id: 2,
+        total_price: 150.49,
+        delivery_address: 'Rua Dois',
+        delivery_number: 'casa 2',
+        status: 'Preparando',
+      },
+    ], {});
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.bulkDelete('Sales', null, {});
+  }
+};

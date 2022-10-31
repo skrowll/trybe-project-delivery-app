aaff7037317142c23a34bd9e028abb89f5b82bbf
Andre Rousselet <andre098@hotmail.com>
2022-10-31 14:17:05 -0300

    feat: função de registro com axios

---
+++ front-end/src/services/requests.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001',
+});
+
+export const setToken = (token) => {
+  api.defaults.headers.common.Authorization = token;
+};
+
+export const requestLogin = async (endpoint, body) => {
+  const { data } = await api.post(endpoint, body);
+  return data;
+};
+
+export const requestRegister = async (endpoint, body) => {
+  const { data } = await api.post(endpoint, body);
+  return data;
+};
+
+export default api;

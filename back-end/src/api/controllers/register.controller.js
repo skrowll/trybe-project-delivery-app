f6935f3d02257e9f1e1e3551b680fe6b7e22383a
Adolligit <adelson.onod@gmail.com>
2022-10-31 17:15:04 -0300

    feat: (Register) [POST, '/'] agora retorna o 'role' e o 'token'

--- back-end/src/api/controllers/register.controller.js
+++ back-end/src/api/controllers/register.controller.js
@@ -3,7 +3,9 @@ const SvcRegister = require('../services/register.service');
 const create = async (req, res) => {
   const created = await SvcRegister.create(req.body);
 
-  return res.status(201).json({ created });
+  console.log(created);
+
+  return res.status(201).json(created);
 };
 
 module.exports = { create };
--- back-end/src/api/services/register.service.js
+++ back-end/src/api/services/register.service.js
@@ -1,14 +1,27 @@
 const md5 = require('md5');
-const { User } = require('../../database/models');
+const { users } = require('../../database/models');
+const { configAuthorization } = require('../../utils/Auth');
 
 const create = async (payload) => {
   const { email, password } = payload;
 
-  const existingUser = await User.findOne({ where: { email, password } });
+  const existingUser = await users.findOne({ where: { email, password } });
 
   if (existingUser) throw new Error('User already registered', { cause: { status: 409 } });
+  
+  const created = await users.create({ ...payload, password: md5(password) });
 
-  return User.create({ ...payload, password: md5(password) });
+  const createdUser = await users.findOne({ where: { email: created.email } });
+
+  console.log('Service  1:', createdUser);
+
+  const { password:_, ...createdUserWithoutPassword } = createdUser;
+  
+  console.log('Service  2:', createdUserWithoutPassword);
+
+  const token = configAuthorization.signAuth(createdUserWithoutPassword);
+
+  return { role: createdUser.role, token };
 };
 
 module.exports = { create };

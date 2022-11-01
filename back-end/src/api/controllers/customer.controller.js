f95f2c68c7b33e8d91431a78ca028690905ee56e
cacau <anaclaudia.sza@outlook.com>
2022-11-01 19:28:08 -0300

    feat(task): typeError

--- back-end/src/api/controllers/customer.controller.js
+++ back-end/src/api/controllers/customer.controller.js
@@ -1,11 +1,21 @@
+const HttpStatus = require('../../utils/HttpStatus');
 const customerService = require('../services/customer.service');
 
 const customerPath = async (_req, res) => {
   const products = await customerService.customerPath();
 
-  return res.status(200).json(products);
+  return res.status(HttpStatus.OK).json(products);
+};
+
+const createOrder = async (req, res) => {
+  const saleInfo = req.body;
+  // const order = await customerService.createOrder(saleInfo);
+  await customerService.createOrder(saleInfo);
+
+  // return res.status(HttpStatus.CREATED).json(order);
 };
 
 module.exports = {
   customerPath,
+  createOrder,
 };
--- back-end/src/api/routes/customer.routes.js
+++ back-end/src/api/routes/customer.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const CustomerController = require('../controllers/customer.controller');
+// const validateToken = require('../middlewares/auth.middleware');
 
 const customer = express();
 
 customer.get('/products', CustomerController.customerPath);
-// customer.post('/checkout', );
+customer.post('/checkout', CustomerController.createOrder);
 
 module.exports = customer;

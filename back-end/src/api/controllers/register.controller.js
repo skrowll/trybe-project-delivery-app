411d369ebf256e96e814e82f3b2929a589cdbc96
adson_gomes_oliveira <contato@devago.com.br>
2022-11-01 05:10:50 -0300

    fix: Corrigindo conflitos no backend, relacionados ao tratamento de erros. Admin Services.

--- back-end/src/api/controllers/register.controller.js
+++ back-end/src/api/controllers/register.controller.js
@@ -9,7 +9,7 @@ const create = async (req, res) => {
 const adminCreate = async (req, res) => {
   const created = await SvcRegister.adminCreate(req.body);
 
-  return res.status(201).json(created);
+  return res.status(HttpStatus.CREATED).json(created);
 };
 
 module.exports = { create, adminCreate };
--- back-end/src/api/middlewares/auth.middleware.js
+++ back-end/src/api/middlewares/auth.middleware.js
@@ -1,19 +1,18 @@
 const { configAuthorization } = require('../../utils/Auth');
+const HttpStatus = require('../../utils/HttpStatus');
 
 const errorMessage = 'Token must be a valid token';
 
 const validateToken = (req, _res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization) throw new Error(errorMessage);
+  if (!authorization) throw new Error(errorMessage, { cause: { status: HttpStatus.UNAUTHORIZED } });
 
   try {
     const { data: { user: { role } } } = configAuthorization.verifyAuth(authorization);
-    if (role !== 'administrator') throw new Error(errorMessage);
-    console.log(role);
+    if (role !== 'administrator') throw new Error(errorMessage, { cause: { status: HttpStatus.UNAUTHORIZED } });
   } catch (error) {
-    console.log(error);
-    throw new Error(errorMessage);
+    throw new Error(errorMessage, { cause: { status: HttpStatus.UNAUTHORIZED } });
   }
   next();
 };

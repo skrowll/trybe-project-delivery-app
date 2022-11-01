bccc6beb563f62dba3735ed8c03c9a118819112f
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-01 01:54:01 -0300

    feat(back-end): cria o arquivo de rota para a rota '/admin'

---
+++ back-end/src/api/routes/admin.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const RegisterController = require('../controllers/register.controller');
+const authMiddleware = require('../middlewares/auth.middleware')
+const RegisterMiddleware = require('../middlewares/register.middleware');
+
+const admin = express();
+
+admin.post('/manage', authMiddleware, RegisterMiddleware.validateInputs, RegisterController.adminCreate);
+
+module.exports = admin;

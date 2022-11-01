7d4e75fd845d08ca29f0a5b0ef88b92b631af2aa
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-01 02:06:20 -0300

    fix: erro de lint

--- back-end/src/api/middlewares/auth.middleware.js
+++ back-end/src/api/middlewares/auth.middleware.js
@@ -1,17 +1,19 @@
 const { configAuthorization } = require('../../utils/Auth');
 
+const errorMessage = 'Token must be a valid token';
+
 const validateToken = (req, _res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization) throw new Error('Token must be a valid token');
+  if (!authorization) throw new Error(errorMessage);
 
   try {
     const { data: { user: { role } } } = configAuthorization.verifyAuth(authorization);
-    if (role !== 'administrator') throw new Error('Token must be a valid token');
+    if (role !== 'administrator') throw new Error(errorMessage);
     console.log(role);
   } catch (error) {
-    console.log(error);
-    throw new Error('Token must be a valid token');
+    console.log(error)
+    throw new Error(errorMessage);
   };
   next();
 };
--- back-end/src/api/routes/admin.routes.js
+++ back-end/src/api/routes/admin.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const RegisterController = require('../controllers/register.controller');
-const authMiddleware = require('../middlewares/auth.middleware')
+const authMiddleware = require('../middlewares/auth.middleware');
 const RegisterMiddleware = require('../middlewares/register.middleware');
 
 const admin = express();
 
-admin.post('/manage', authMiddleware, RegisterMiddleware.validateInputs, RegisterController.adminCreate);
+admin.post(
+  '/manage',
+  authMiddleware,
+  RegisterMiddleware.validateInputs,
+  RegisterController.adminCreate,
+);
 
 module.exports = admin;

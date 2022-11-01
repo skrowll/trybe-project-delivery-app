bb92c4d749757116f7d13c1df40c2c4dcf263548
Andre Rousselet <andre098@hotmail.com>
2022-10-31 21:16:09 -0300

    refactor: padroniza lançamento de erro

--- back-end/src/api/services/login.service.js
+++ back-end/src/api/services/login.service.js
@@ -5,23 +5,23 @@ const { configAuthorization } = require('../../utils/Auth');
 const loginUser = async ({ password, email }) => {
   const user = await users.findOne({ where: { email } });
 
-  if (!user) throw new Error('Invalid email');
+  if (!user) throw new Error('Invalid email', { cause: { status: 404 } });
 
   const validPass = md5(password) === user.password;
 
-  if (!validPass) {
-    const error = new Error('Invalid password');
-    error.status = 400;
-    throw error;
-  }
+  if (!validPass) throw new Error('Invalid password', { cause: { status: 400 } });
+  //   const error = new Error('Invalid password');
+  //   error.status = 400;
+  //   throw error;
+  // }
   
   const token = configAuthorization.signAuth(user);
 
-  if (!token) {
-    const error = new Error('Invalid token');
-    error.status = 400;
-    throw error;
-  }
+  if (!token) throw new Error('Invalid token', { cause: { status: 400 } });
+  //   const error = new Error('Invalid token');
+  //   error.status = 400;
+  //   throw error;
+  // }
 
   return { name: user.name, email: user.email, role: user.role, token };
 };

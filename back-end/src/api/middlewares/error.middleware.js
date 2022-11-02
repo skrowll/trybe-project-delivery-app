80c56ea072b8e9042257e29f1bfc03b007c6942a
Adolligit <adelson.onod@gmail.com>
2022-11-02 00:27:26 -0300

    fix: (Middle. Error) agora é feita uma verificação da chave 'cause' antes de utilizar 'status'

--- back-end/src/api/middlewares/error.middleware.js
+++ back-end/src/api/middlewares/error.middleware.js
@@ -1,6 +1,6 @@
 const HttpStatus = require('../../utils/HttpStatus');
 
 const errorMiddleware = (error, _req, res, _next) => res
-  .status(error.cause.status || HttpStatus.INTERNAL).json({ error: error.message });
+  .status(error.cause?.status || HttpStatus.INTERNAL).json({ error: error.message });
   
 module.exports = errorMiddleware;

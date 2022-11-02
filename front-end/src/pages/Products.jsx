b4107b409eedcb06a50d7feaa044a2a8d34bc22b
adson_gomes_oliveira <contato@devago.com.br>
2022-11-02 03:22:07 -0300

    feat: Renderizando cards de produtos

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,17 +1,38 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useCallback } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import DeliveryContext from '../context/DeliveryContext';
 import { requestProducts } from '../services/requests';
 
 function Products() {
-  const { setProducts } = useContext(DeliveryContext);
+  const { products, setProducts } = useContext(DeliveryContext);
 
-  useEffect(() => {
-    const productsList = requestProducts('/customer/products');
+  const fetchProducts = useCallback(async () => {
+    const productsList = await requestProducts('/customer/products');
     setProducts(productsList);
   }, [setProducts]);
 
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
   return (
-    <h1>OI</h1>
+    <section className="product-cards">
+      {products.length > 0 && products.map((prod) => {
+        const { id, name, price, urlImage } = prod;
+
+        return (
+          <div
+            key={ uuidv4() }
+            data-testid={ `customer_products__element-card-price-${id}` }
+          >
+            <img src={ urlImage } alt={ name } />
+            <span>{name}</span>
+            <span>{price}</span>
+          </div>
+        );
+      })}
+    </section>
   );
 }
 

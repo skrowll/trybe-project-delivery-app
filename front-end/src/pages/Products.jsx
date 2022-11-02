cb2937a88b74092394866fb4abef1c048886b0fa
adson_gomes_oliveira <contato@devago.com.br>
2022-11-02 04:45:29 -0300

    feat: Criando os controles em cada card, juntamente a suas lógicas ( incompleto )

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useCallback } from 'react';
+import { useContext, useEffect, useCallback, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import DeliveryContext from '../context/DeliveryContext';
@@ -6,6 +6,8 @@ import { requestProducts } from '../services/requests';
 
 function Products() {
   const { products, setProducts } = useContext(DeliveryContext);
+  const [quantity, setQuantity] = useState(0);
+  const [cartState, setCartState] = useState([]);
 
   const fetchProducts = useCallback(async () => {
     const productsList = await requestProducts('/customer/products');
@@ -15,6 +17,47 @@ function Products() {
   useEffect(() => {
     fetchProducts();
   }, [fetchProducts]);
+
+  useEffect(() => {
+    localStorage.setItem('carrinho', JSON.stringify(cartState));
+  }, [cartState]);
+
+  const handleCardControls = (event, product) => {
+    event.preventDefault();
+    const { name } = event.target;
+    if (name === 'add_button') {
+      setQuantity(Number(quantity) + 1);
+      return setCartState([...cartState, {
+        id: product.id,
+        name: product.name,
+        urlImage: product.urlImage,
+        quantity,
+      }]);
+    }
+    if (name === 'rm_button' && quantity - 1 >= 0) {
+      setQuantity(Number(quantity) - 1);
+      return setCartState([...cartState, {
+        id: product.id,
+        name: product.name,
+        urlImage: product.urlImage,
+        quantity,
+      }]);
+    }
+  };
+
+  const handleCardChange = (event, product) => {
+    event.preventDefault();
+    const { value } = event.target;
+    if (value >= 0) {
+      setQuantity(value);
+      return setCartState([...cartState, {
+        id: product.id,
+        name: product.name,
+        urlImage: product.urlImage,
+        quantity,
+      }]);
+    }
+  };
 
   return (
     <section className="product-cards">
@@ -42,21 +85,27 @@ function Products() {
             </span>
             <div className="card-controls">
               <button
+                name="add_button"
                 type="button"
                 data_testid={ `customer_products__button-card-add-item-${id}` }
+                onClick={ (e) => handleCardControls(e, prod) }
               >
                 +
               </button>
               <label htmlFor="item_quantity">
                 <input
-                  id="item_quantity"
-                  type="text"
+                  name="item_quantity"
+                  type="number"
+                  min="0"
                   data_testid={ `customer_products__input-card-quantity-${id}` }
+                  onChange={ (e) => handleCardChange(e, prod) }
                 />
               </label>
               <button
+                name="rm_button"
                 type="button"
                 data_testid={ `customer_products__button-card-rm-item-${id}` }
+                onClick={ (e) => handleCardControls(e, prod) }
               >
                 -
               </button>
@@ -69,3 +118,27 @@ function Products() {
 }
 
 export default Products;
+
+// setQuantity(Number(quantity) - 1);
+//       const oldCartStored = JSON.parse(localStorage.getItem('carrinho'));
+
+//       const cartElementIndex = oldCartStored
+//         .findIndex((prod) => prod.id === product.id);
+
+//       if (cartElementIndex) {
+//         const actualCart = cartState;
+//         actualCart[cartElementIndex] = {
+//           id: product.id,
+//           name: product.name,
+//           urlImage: product.urlImage,
+//           quantity,
+//         };
+//         return setCartState([...actualCart]);
+//       }
+
+//       return setCartState([...cartState, {
+//         id: product.id,
+//         name: product.name,
+//         urlImage: product.urlImage,
+//         quantity,
+//       }]);

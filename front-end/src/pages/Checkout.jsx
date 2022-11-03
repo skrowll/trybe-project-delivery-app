067eae5df5ff78a9340e293098b9ef475b4422a7
Adolligit <adelson.onod@gmail.com>
2022-11-03 20:40:50 -0300

    feat: (Customer: Checkout) [frontend] inserindo os 'data-testids' nos elementos

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -39,14 +39,24 @@ function Checkout() {
 
   const removeItem = (id, list) => list.filter(({ id: idFilter }) => idFilter !== id);
 
-  const createListTable = (({ id, name, quantity, price }, indexLine) => (
+  const createListTable = (({ id, name, quantity, price }, index) => (
     <>
-      <td>{ indexLine }</td>
-      <td>{ name }</td>
-      <td>{ quantity }</td>
-      <td>{ `R$${price}` }</td>
-      <td>{ `R$${(price * quantity).toFixed(2)}` }</td>
-      <td>
+      <td data-testid={ `customer_checkout__element-order-table-item-number-${index}` }>
+        { index + 1 }
+      </td>
+      <td data-testid={ `customer_checkout__element-order-table-name-${index}` }>
+        { name }
+      </td>
+      <td data-testid={ `customer_checkout__element-order-table-quantity-${index}` }>
+        { quantity }
+      </td>
+      <td data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }>
+        { `R$${price}` }
+      </td>
+      <td data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }>
+        { `R$${(price * quantity).toFixed(2)}` }
+      </td>
+      <td data-testid={ `customer_checkout__element-order-table-remove-${index}` }>
         <input
           type="button"
           value="remover"
@@ -74,32 +84,29 @@ function Checkout() {
           <tbody>
             {
               products.map((product, index) => (
-                <tr
-                  key={ index }
-                  data-testid={ `element-order-table-name-${index}` }
-                >
-                  { createListTable(product, index + 1) }
+                <tr key={ index }>
+                  { createListTable(product, index) }
                 </tr>
               ))
             }
           </tbody>
         </table>
-        <span>{ `Total: R$${(total).toFixed(2)}` }</span>
+        <span data-testid="customer_checkout__element-order-total-price">
+          { `Total: R$${(total).toFixed(2)}` }
+        </span>
       </fieldset>
       <h2>Detalhe e Endereço para Entrega</h2>
       <fieldset>
         <span>Vendedor(a)</span>
         <select
+          data-testid="customer_checkout__select-seller"
           id="sellers"
           name="sellers"
           onChange={ ({ target: { value } }) => setSeller(value) }
         >
           {
             listSeller.map((name) => (
-              <option
-                key={ name }
-                value={ name }
-              >
+              <option key={ name } value={ name }>
                 { name }
               </option>
             ))
@@ -107,12 +114,14 @@ function Checkout() {
         </select>
         <span>Endereço</span>
         <input
+          data-testid="customer_checkout__input-address"
           type="text"
           value={ address }
           onChange={ ({ target: { value } }) => setAddress(value) }
         />
         <span>Número</span>
         <input
+          data-testid="customer_checkout__input-address-number"
           type="number"
           value={ numberAddress }
           onChange={ ({ target: { value } }) => setNumberAddress(value) }

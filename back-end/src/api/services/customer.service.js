771b2cdb042201ce46c429dc44a3fe89ee563260
cacau <anaclaudia.sza@outlook.com>
2022-11-04 16:07:59 -0300

    fix: error lint

--- back-end/src/api/services/customer.service.js
+++ back-end/src/api/services/customer.service.js
@@ -11,11 +11,11 @@ const customerPath = async () => {
   return getProducts;
 };
 
-const createOrder = async ({ sale, products }) => {
+const createOrder = async ({ sale, produtos }) => {
   const newSale = await sequelize.transaction(async (transaction) => {
     const order = await sales.create(sale, { transaction });
 
-    const saleProduct = await products.map((p) => (
+    const saleProduct = await produtos.map((p) => (
       { saleId: order.id, productId: p.id, quantity: p.quantity }
     ));
 
@@ -25,9 +25,39 @@ const createOrder = async ({ sale, products }) => {
   });
 
   return newSale;
-}
+};
 
 module.exports = {
   customerPath,
   createOrder,
 };
+
+// {
+//   "sale": {
+//     "userId": 1,
+//     "sellerId": 1,
+//     "totalPrice": 4.99,
+//     "deliveryAddress": "A Casa ali da Esquina",
+//     "deliveryNumber": 5
+//   },
+//   "produtos": [
+//     {
+//       "id": 1,
+//       "name": "Skol Lata 250ml",
+//       "price": 2.20,
+//       "quantity": 2
+//     },
+//     {
+//       "id": 2,
+//       "name": "Brahma Duplo Malte 350ml",
+//       "price": 2.79,
+//       "quantity": 1
+//     },
+//     {
+//       "id": 11,
+//       "name": "Stella Artois 275ml",
+//       "price": "3.49",
+//       "quantity": 1
+//     }
+//   ]
+// }

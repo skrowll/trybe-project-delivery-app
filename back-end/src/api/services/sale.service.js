4a799d8c3e1906c14138048fe315c53433b7a8b0
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-07 17:30:48 -0300

    feat(back-end): cria sale.service

---
+++ back-end/src/api/services/sale.service.js
@@ -0,0 +1,21 @@
+const { users, sales } = require('../../database/models');
+const HttpStatus = require('../../utils/HttpStatus');
+const { configAuthorization } = require('../../utils/Auth')
+
+const getSalesBySellerId = async ({ authorization }) => {
+  const { data: { user: { id, role } } } = configAuthorization.verifyAuth(authorization);
+
+  if (role !== 'seller') {
+    throw new Error('User must be a seller', { cause: { status: HttpStatus.UNAUTHORIZED } });
+  }
+
+  const salesBySellerId = await sales.findAll({ where: { sellerId: id } });
+
+  if (salesBySellerId.length === 0) {
+    throw new Error('Sales not found', { cause: { status: HttpStatus.NOT_FOUND } });
+  }
+
+  return salesBySellerId;
+};
+
+module.exports = { getSalesBySellerId };

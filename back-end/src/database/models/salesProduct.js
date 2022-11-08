05c1e8091b3d9f1d806fc0439d7268aff77847d1
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-08 18:32:41 -0300

    fix(back-end): correção do associate da model de salesProduct

--- back-end/src/database/models/salesProduct.js
+++ back-end/src/database/models/salesProduct.js
@@ -16,14 +16,14 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   SalesProducts.associate = (models) => {
-    models.sales.belongsToMany(models.products, {
-      as: 'products',
+    models.products.belongsToMany(models.sales, {
+      as: 'sales',
       through: SalesProducts,
       foreignKey: 'productId',
       otherKey: 'saleId',
     });
-    models.products.belongsToMany(models.sales, {
-      as: 'sales',
+    models.sales.belongsToMany(models.products, {
+      as: 'products',
       through: SalesProducts,
       foreignKey: 'saleId',
       otherKey: 'productId',

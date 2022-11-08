347a7c16b322895c441335742bc297a085059e51
Adolligit <adelson.onod@gmail.com>
2022-11-07 21:29:38 -0300

    chore: mudança no nome do 'data-testid' do botão de remover a quantidade de um produto

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -53,7 +53,7 @@ function Products() {
         onClick={ () => navigate('/customer/checkout') }
         data-testid="customer_products__checkout-bottom-value"
       >
-        <span>{cartTotalPrice}</span>
+        <span>{cartTotalPrice.toFixed(2).replace('.', ',')}</span>
       </button>
       <section className="product-cards">
         {
@@ -73,7 +73,7 @@ function Products() {
               </h3>
               <div className="card-controls">
                 <button
-                  data-testid={ `customer_products__button-card-add-item-${id}` }
+                  data-testid={ `customer_products__button-card-rm-item-${id}` }
                   onClick={ ({ target }) => buttonHandler(target, index) }
                   name={ name }
                   type="button"
@@ -95,6 +95,14 @@ function Products() {
                   name={ name }
                   type="button"
                   value="+"
+                >
+                  +
+                </button>
+                <button
+                  data-testid={ `customer_products__button-card-add-item-${id}` }
+                  onClick={ (e) => handleButtonChange(e, prod) }
+                  name="add_button"
+                  type="button"
                 >
                   +
                 </button>

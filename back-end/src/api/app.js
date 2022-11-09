e9ca19d2d40a21a6144b9877f49b16c48099286d
Andre Rousselet <andre098@hotmail.com>
2022-11-09 13:17:31 -0300

    feat: adiciona autenticação nas rotas de customer

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,7 @@ require('express-async-errors');
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const auth = require('./middlewares/auth.middleware');
 const errorMiddleware = require('./middlewares/error.middleware');
 
 const app = express();
@@ -14,7 +15,7 @@ app.use('/login', routes.login);
 app.use('/admin', routes.admin);
 app.use('/user', routes.user);
 app.use('/register', routes.register);
-app.use('/customer', routes.customer);
+app.use('/customer', auth.validateCustomerToken, routes.customer);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 

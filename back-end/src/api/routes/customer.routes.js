f5619741209dea67269233ce0e20e2156c8ff081
Andre Rousselet <andre098@hotmail.com>
2022-11-09 15:29:36 -0300

    fix: renomeia componente

--- back-end/src/api/routes/customer.routes.js
+++ back-end/src/api/routes/customer.routes.js
@@ -6,7 +6,6 @@ const customer = express();
 
 customer.get('/products', CustomerController.customerPath);
 customer.post('/checkout', validateCustomerToken, CustomerController.createOrder);
-customer.post('/checkout', CustomerController.createOrder);
 customer.get('/orders', CustomerController.getOrders);
 
 module.exports = customer;

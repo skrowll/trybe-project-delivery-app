0d7b50a5625aedc0e91ed810a2f5339c60926f12
Andre Rousselet <andre098@hotmail.com>
2022-11-09 11:00:46 -0300

    style: adiciona estilo minimo no card de pedidos

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -6,14 +6,27 @@ function OrderCard({ order }) {
   const date = new Date(saleDate).toLocaleDateString();
 
   return (
-    <>
+    <div
+      style={
+        { alignItems: 'center',
+          border: 'solid 1px black',
+          display: 'flex',
+          gap: '20px',
+          justifyContent: 'space-between',
+          marginBottom: '10px',
+          padding: '0 10px',
+          width: '300px' }
+      }
+    >
       <p data-testid={ `customer_orders__element-order-id-${id}` }>{ `Pedido ${id}` }</p>
       <p data-testid={ `customer_orders__element-delivery-status-${id}` }>{ status }</p>
-      <p data-testid={ `customer_orders__element-order-date-${id}` }>{ date }</p>
-      <p data-testid={ `customer_orders__element-card-price-${id}` }>
-        { `R$ ${Number(totalPrice)}` }
-      </p>
-    </>
+      <div>
+        <p data-testid={ `customer_orders__element-order-date-${id}` }>{ date }</p>
+        <p data-testid={ `customer_orders__element-card-price-${id}` }>
+          { `R$ ${Number(totalPrice)}` }
+        </p>
+      </div>
+    </div>
   );
 }
 

8764f6d83289777550b560c6c6a335591660e502
Andre Rousselet <andre098@hotmail.com>
2022-11-09 10:26:58 -0300

    feat: adiciona rota de pedidos do cliente no App

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,10 +2,11 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import React from 'react';
 import Products from './pages/Products';
 import Register from './pages/Register';
-import './App.css';
 import Login from './pages/Login';
 import Manage from './pages/Manage';
 import Checkout from './pages/Checkout';
+import Orders from './pages/Orders';
+import './App.css';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <Products /> } />
       <Route path="/customer/checkout" element={ <Checkout /> } />
+      <Route path="/customer/orders" element={ <Orders /> } />
       <Route path="/admin/manage" element={ <Manage /> } />
       <Route path="/login" element={ <Login /> } />
       <Route exact path="/" element={ <Navigate to="/login" replace /> } />

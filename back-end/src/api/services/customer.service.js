42ec47587d2e34015fb4d27f452deaeffb1b9b44
cacau <anaclaudia.sza@outlook.com>
2022-11-09 13:14:01 -0300

    feat: requisicao checkout

--- back-end/src/api/services/customer.service.js
+++ back-end/src/api/services/customer.service.js
@@ -11,11 +11,11 @@ const customerPath = async () => {
   return getProducts;
 };
 
-const createOrder = async ({ sale, produtos }) => {
+const createOrder = async ({ sale, cart }) => {
   const newSale = await sequelize.transaction(async (transaction) => {
     const order = await sales.create(sale, { transaction });
 
-    const saleProduct = await produtos.map((p) => (
+    const saleProduct = await cart.map((p) => (
       { saleId: order.id, productId: p.id, quantity: p.quantity }
     ));
 
@@ -24,40 +24,10 @@ const createOrder = async ({ sale, produtos }) => {
     return order;
   });
 
-  return newSale;
+  return newSale.id;
 };
 
 module.exports = {
   customerPath,
   createOrder,
 };
-
-// {
-//   "sale": {
-//     "userId": 1,
-//     "sellerId": 1,
-//     "totalPrice": 4.99,
-//     "deliveryAddress": "A Casa ali da Esquina",
-//     "deliveryNumber": 5
-//   },
-//   "produtos": [
-//     {
-//       "id": 1,
-//       "name": "Skol Lata 250ml",
-//       "price": 2.20,
-//       "quantity": 2
-//     },
-//     {
-//       "id": 2,
-//       "name": "Brahma Duplo Malte 350ml",
-//       "price": 2.79,
-//       "quantity": 1
-//     },
-//     {
-//       "id": 11,
-//       "name": "Stella Artois 275ml",
-//       "price": "3.49",
-//       "quantity": 1
-//     }
-//   ]
-// }
--- front-end/src/components/CardAdressDetails.jsx
+++ front-end/src/components/CardAdressDetails.jsx
@@ -1,64 +1,56 @@
 import propTypes from 'prop-types';
 import { useEffect, useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { request } from '../services/requests';
 
 function CardAdressDetails({ cart, cartTotalPrice }) {
-  // const navigate = useNavigate();
-  const [seller, setSeller] = useState('');
+  const navigate = useNavigate();
   const [address, setAddress] = useState('');
+  const [sellerId, setSellerId] = useState('');
   const [number, setNumberAddress] = useState(0);
   const [listSeller, setListSeller] = useState([]);
-
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    const checkout = {
-      sale: {
-        userId: user.id,
-        sellerId: 1,
-        totalPrice: cartTotalPrice,
-        deliveryAddress: address,
-        deliveryNumber: Number(number),
-      },
-      cart,
-    };
-    console.log(checkout);
-  }, [address, number, cart, cartTotalPrice]);
 
   useEffect(() => request('get', '/user?role=seller')
     .then((data) => {
       setListSeller(data.map((e) => e));
     }));
 
-  const sendRequest = () => {
-    console.log('meh');
-    // const response = request('get', '/customer/checkout', { checkout })
-    //   .then((data) => {
-    //     setListSeller(data.map((e) => e));
-    //   });
-
-    // console.log(response);
-    // navigate('/customer/orders/<id>');
+  const submitCheckout = async () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const checkout = {
+      sale: {
+        sellerId,
+        userId: user.id,
+        totalPrice: cartTotalPrice,
+        deliveryAddress: address,
+        deliveryNumber: Number(number),
+      },
+      cart,
+    };
+    const order = await request('post', '/customer/checkout', checkout);
+    navigate(`/customer/orders/${order}`);
+    // TODO: autorização mesmo depois de atualizar página
   };
 
   return (
     <>
       <h2>Detalhe e Endereço para Entrega</h2>
       <fieldset>
-        <span>Vendedor(a)</span>
+        <span>P. Vendedora Responsável</span>
         <select
           id="sellers"
           name="sellers"
           data-testid="customer_checkout__select-seller"
-          onClick={ ({ target: { value } }) => setSeller(value) }
+          onClick={ ({ target: { value } }) => setSellerId(value) }
         >
+          <option>Selecione</option>
           {listSeller.length > 0
-            && listSeller.map(({ name }) => (
+            && listSeller.map((item, index) => (
               <option
-                key={ name }
-                value={ name }
+                key={ index }
+                value={ item.id }
               >
-                {name}
+                {item.name}
               </option>
             ))}
         </select>
@@ -78,24 +70,18 @@ function CardAdressDetails({ cart, cartTotalPrice }) {
         />
         <button
           type="button"
-          // value="Finalizar Pedido"
-          onClick={ sendRequest }
+          onClick={ submitCheckout }
           data-testid="customer_checkout__button-submit-order"
         >
           Finalizar Pedido
         </button>
       </fieldset>
-      <span
-        onChange={ ({ target: { value } }) => setSeller(value) }
-      >
-        {seller}
-      </span>
     </>
   );
 }
 
 CardAdressDetails.propTypes = {
-  cart: propTypes.arrayOf().isRequired,
+  cart: propTypes.arrayOf.isRequired,
   cartTotalPrice: propTypes.number.isRequired,
 };
 

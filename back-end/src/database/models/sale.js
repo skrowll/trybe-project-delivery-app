1d6500ff428173e60315aa52be6910bfaa9abfcd
Andre Rousselet <andre098@hotmail.com>
2022-11-09 10:32:05 -0300

    refactor: adiciona underscored no model sales e valor default em 'status'

--- back-end/src/database/models/sale.js
+++ back-end/src/database/models/sale.js
@@ -14,8 +14,12 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW,
     },
-    status: DataTypes.STRING,
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: 'Pendente',
+    }
   }, {
+    underscored: true,
     tableName: 'sales',
     timestamps: false
   });

8b244df0cb8865999d5444954126eee179ce56c6
Andre Rousselet <andre098@hotmail.com>
2022-11-09 17:28:33 -0300

    refactor: remove blocos try/catch e passa id através de res.locals

--- back-end/src/api/controllers/sale.controller.js
+++ back-end/src/api/controllers/sale.controller.js
@@ -1,22 +1,15 @@
 const SaleService = require('../services/sale.service');
 const HttpStatus = require('../../utils/HttpStatus');
 
-const getSalesBySellerId = async (req, res, next) => {
-  try {
-    const result = await SaleService.getSalesBySellerId(req.headers);
-    return res.status(HttpStatus.OK).json(result);
-  } catch (error) {
-    next(error);
-  }
+const getSalesBySellerId = async (req, res) => {
+  const sellerId = res.locals.user.id;
+  const result = await SaleService.getSalesBySellerId(sellerId);
+  return res.status(HttpStatus.OK).json(result);
 };
 
-const getSaleById = async (req, res, next) => {
-  try {
-    const result = await SaleService.getSaleById(req.params);
-    return res.status(HttpStatus.OK).json(result);
-  } catch (error) {
-    next(error);
-  }
+const getSaleById = async (req, res) => {
+  const result = await SaleService.getSaleById(req.params);
+  return res.status(HttpStatus.OK).json(result);
 };
 
 module.exports = { getSalesBySellerId, getSaleById };

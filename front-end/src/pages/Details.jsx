7559b8d5ac3008913ef950121111c8f68d738d9c
Andre Rousselet <andre098@hotmail.com>
2022-11-09 21:14:29 -0300

    feat: retira contexto e usa estado para armazenar pedido especifico
    componentiza cada produto do pedido (OrderedProductsDetails) e adiciona estilo à tabela

--- front-end/src/pages/Details.jsx
+++ front-end/src/pages/Details.jsx
@@ -1,16 +1,10 @@
-import { useEffect, useContext } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
-import OrderDetailsHeader from '../components/OrderDetailsHeader';
-import DeliveryContext from '../context/DeliveryContext';
+import DetailsHeader from '../components/DetailsHeader';
+import OrderedProductsDetails from '../components/OrderedProductsDetails';
 import { request, setToken } from '../services/requests';
 
 const dataTestIds = {
-  itemNumber: 'seller_order_details__element-order-table-item-number-',
-  tableName: 'seller_order_details__element-order-table-name-',
-  tableQuantity: 'seller_order_details__element-order-table-quantity-',
-  unitPrice: 'seller_order_details__element-order-table-unit-price-',
-  subTotal: 'seller_order_details__element-order-table-sub-total-',
   totalPrice: 'seller_order_details__element-order-total-price',
 };
 
@@ -24,7 +18,8 @@ const tableHeaderCols = [
 
 function Details() {
   const { id } = useParams();
-  const { values: { orders }, functions: { setOrders } } = useContext(DeliveryContext);
+  const [order, setOrder] = useState({});
+  const { products } = order;
   const { token, role } = JSON.parse(localStorage.getItem('user'));
 
   useEffect(() => {
@@ -32,67 +27,37 @@ function Details() {
     const url = role === 'customer' ? '/customer/orders' : '/seller/orders';
     const requestData = async () => {
       const data = await request('get', `${url}/${id}`);
-      console.log(data);
-      setOrders(data);
+      setOrder(data);
     };
     requestData();
-  }, [id, token, role, setOrders]);
+  }, [id, token, role, setOrder]);
 
   return (
     <section>
       <h1>Detalhe do Pedido</h1>
       <div>
-        <OrderDetailsHeader order={ orders } role={ role } />
-        <table>
+        <DetailsHeader order={ order } role={ role } />
+        <table style={ { width: '90%', margin: '0 auto', textAlign: 'center' } }>
           <thead>
             <tr>
-              { tableHeaderCols.map((col) => (<th key={ col }>{col}</th>))}
+              { tableHeaderCols.map((col) => (<th key={ col }>{col}</th>)) }
             </tr>
           </thead>
           <tbody>
-            {orders.products?.map((product, index) => (
-              <tr
-                key={ uuidv4() }
-              >
-                <td
-                  data-testid={ `${dataTestIds.itemNumber}${index}` }
-                >
-                  {product.id}
-                </td>
-                <td
-                  data-testid={ `${dataTestIds.tableName}${index}` }
-                >
-                  {product.name}
-                </td>
-                <td
-                  data-testid={ `${dataTestIds.tableQuantity}${index}` }
-                >
-                  {product.salesProducts.quantity}
-                </td>
-                <td
-                  data-testid={ `${dataTestIds.itemNumber}${index}` }
-                >
-                  {product.price}
-                </td>
-                <td
-                  data-testid={ `${dataTestIds.subTotal}${index}` }
-                >
-                  {product.salesProducts.quantity * product.price}
-                </td>
+            { products?.map((product) => (
+              <tr key={ product.id }>
+                <OrderedProductsDetails product={ product } />
               </tr>
             ))}
           </tbody>
         </table>
-        {orders
-          && (
-            <div>
-              <span
-                data-testid={ dataTestIds.totalPrice }
-              >
-                { `R$ ${orders.totalPrice}` }
-              </span>
-            </div>
-          )}
+        <div>
+          <span
+            data-testid={ dataTestIds.totalPrice }
+          >
+            { `R$ ${Number(order.totalPrice).toFixed(2).replace('.', ',')}` }
+          </span>
+        </div>
       </div>
     </section>
   );
